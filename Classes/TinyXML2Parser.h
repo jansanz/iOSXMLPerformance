934db8e5aece9e0f9f9b2220159b4f77bb62173c
Jan Sanchez <janfsd+github@gmail.com>
2012-11-10 16:30:38 -0500

    Added TinyXML2 library

---
+++ Classes/TinyXML2Parser.h
@@ -0,0 +1,13 @@
+//
+//  TinyXML2Parser.h
+//  XMLPerformance
+//
+//  Created by Jan Sanchez on 11/10/12.
+//
+//
+
+#import "iTunesRSSParser.h"
+
+@interface TinyXML2Parser : iTunesRSSParser
+
+@end
---
+++ Classes/TinyXML2Parser.m
@@ -0,0 +1,13 @@
+//
+//  TinyXML2Parser.m
+//  XMLPerformance
+//
+//  Created by Jan Sanchez on 11/10/12.
+//
+//
+
+#import "TinyXML2Parser.h"
+
+@implementation TinyXML2Parser
+
+@end
